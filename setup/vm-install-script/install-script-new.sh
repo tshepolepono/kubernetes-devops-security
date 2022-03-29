837da71edbeeac01a95924971b4b2524591421c6
William Guilherme <wguilherme@securitygeek.io>
2022-03-29 00:16:01 -0700

    Initial Commit

--- setup/vm-install-script/install-script-new.sh
+++ setup/vm-install-script/install-script-new.sh
@@ -8,16 +8,31 @@ source ~/.bashrc
 
 apt-get autoremove -y  #removes the packages that are no longer needed
 apt-get update
+sudo ufw disable
+sudo ufw reset -y
 systemctl daemon-reload
 
+echo ".........----------------#################._.-.-DOCKER-.-._.#################----------------........."
+sudo apt-get install docker.io -y
+sudo systemctl enable docker
+sudo systemctl start docker
+
+cat > /etc/systemd/system/docker.service.d/docker.conf <<EOF
+[Service]
+ExecStart=
+ExecStart=/usr/bin/dockerd
+EOF
+
+echo ".........----------------#################._.-.-KUBERNETES-.-._.#################----------------........."
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
-KUBE_VERSION=1.24.0
 apt-get update
-apt-get install -y kubelet=${KUBE_VERSION}-00 vim build-essential jq python3-pip docker.io kubectl=${KUBE_VERSION}-00 kubernetes-cni=0.8.7-00 kubeadm=${KUBE_VERSION}-00
+sudo apt-get install kubeadm kubelet kubectl -y
+apt-mark hold kubeadm kubelet kubectl
+sudo apt install python3-pip -y
 pip3 install jc
 
 ### UUID of VM
@@ -28,36 +43,35 @@ cat > /etc/docker/daemon.json <<EOF
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
   "log-driver": "json-file",
-  "storage-driver": "overlay2"
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ]
 }
 EOF
 mkdir -p /etc/systemd/system/docker.service.d
 
+sudo swapoff -a
+sudo swapoff -a && sed -i '/swap/d' /etc/fstab
+sudo sed -i '/ swap / s/^/#/' /etc/fstab
+sudo kubeadm init --apiserver-advertise-address=172.31.19.175 --ignore-preflight-errors all --pod-network-cidr=10.244.0.0/16 --token-ttl 0
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+export KUBECONFIG=/etc/kubernetes/admin.conf
+
 systemctl daemon-reload
 systemctl restart docker
-systemctl enable docker
 systemctl enable kubelet
 systemctl start kubelet
 
-echo ".........----------------#################._.-.-KUBERNETES-.-._.#################----------------........."
-rm /root/.kube/config
-kubeadm reset -f
-
-# uncomment below line if your host doesnt have minimum requirement of 2 CPU
-# kubeadm init --kubernetes-version=${KUBE_VERSION} --ignore-preflight-errors=NumCPU --skip-token-print
-kubeadm init --kubernetes-version=${KUBE_VERSION} --skip-token-print
-
-mkdir -p ~/.kube
-sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config
-
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-
-sleep 60
-
-echo "untaint controlplane node"
-kubectl taint node $(kubectl get nodes -o=jsonpath='{.items[].metadata.name}')  node-role.kubernetes.io/master-
-kubectl get node -o wide
-
+echo ".........----------------#################._.-.-DEPLOY POD NETWORK-.-._.#################----------------........."
+sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+kubectl get pods --all-namespaces
 
 
 echo ".........----------------#################._.-.-Java and MAVEN-.-._.#################----------------........."
@@ -65,8 +79,6 @@ sudo apt install openjdk-8-jdk -y
 java -version
 sudo apt install -y maven
 mvn -v
-
-
 
 echo ".........----------------#################._.-.-JENKINS-.-._.#################----------------........."
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
@@ -76,8 +88,9 @@ sudo apt install -y jenkins
 systemctl daemon-reload
 systemctl enable jenkins
 sudo systemctl start jenkins
-#sudo systemctl status jenkins
 sudo usermod -a -G docker jenkins
 echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 echo ".........----------------#################._.-.-COMPLETED-.-._.#################----------------........."
+
+
